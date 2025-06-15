abb919bbdec90b74e55601e255fe3e821574e24f
Chakrit <imnew108@gmail.com>
2025-06-15 15:39:37 +0700

    inv: user input fetching

--- investment_calculator/main.go
+++ investment_calculator/main.go
@@ -12,14 +12,28 @@ func main() {
 	//	years              float64 = 10
 	//)
 	const inflationRate = 2.5
-	var investmentAmount float64
-	years, expectedReturnRate := 10.0, 5.5
+	//var (
+	//	investmentAmount   float64
+	//	years              float64
+	//	expectedReturnRate float64
+	//)
 
+	var investmentAmount, years, expectedReturnRate float64
+
+	//expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Printf("Future Value: %v\n", futureValue) // 1708.1444583535929
+	fmt.Printf("Future Value: %v\n", futureValue)
 	fmt.Printf("Real Future Value: %v\n", futureRealValue)
 
 	//myCal()
