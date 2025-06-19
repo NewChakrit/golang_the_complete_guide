77b52f11b8a8006280ab9b5a8c0cd197c9962c80
657002 <imnew108@gmail.com>
2025-06-19 17:55:55 +0700

    07: interfaces, dynamic types & limitations, introducting generics

--- 07-interface/main.go
+++ 07-interface/main.go
@@ -1,30 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	result := add(2.4, 1)
+	fmt.Println(result)
 }
 
-func add(a, b interface{}) interface{} {
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
+// func add[T any](a, b T) T {
+func add[T int | float64 | string](a, b T) T {
 
-	if aIsInt && bIsInt {
-		return aInt + bInt
-	}
-
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)
-
-	if aIsFloat && bIsFloat {
-		return aFloat + bFloat
-	}
-
-	aString, aIsString := a.(string)
-	bString, bIsString := b.(string)
-
-	if aIsString && bIsString {
-		return aString + bString
-	}
-
-	return nil
+	return a + b
 }
