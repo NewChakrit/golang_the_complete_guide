ce881f85bb83d3453c8ffc6c76a3c11bf49a4c65
657002 <imnew108@gmail.com>
2025-06-20 23:07:50 +0700

    10: returning function as values

--- 10-module/main.go
+++ 10-module/main.go
@@ -6,11 +6,21 @@ type transfromFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transfromerFn1 := getTransformerFunction(&numbers)
+	transfromerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transfromerFn1)
+	moreTransformNumbers := transformNumbers(&moreNumbers, transfromerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformNumbers)
 }
 
 // func transformNumbers(number *[]int, transform func(int) int) []int {
@@ -24,6 +34,15 @@ func transformNumbers(number *[]int, transform transfromFn) []int {
 	}
 
 	return dNumbers
+}
+
+// func getTransformerFunction() func(int) int {
+func getTransformerFunction(numbers *[]int) transfromFn {
+	if (*numbers)[0] == 1 { // ใช้ ()เพราะต่างโคตรสร้าง
+		return double
+	} else {
+		return triple
+	}
 }
 
 func doubleNumbers(number *[]int) []int {
