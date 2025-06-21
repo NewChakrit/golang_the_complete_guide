c0a053489e5f30ce48dcf5698c5f7f4568800955
657002 <imnew108@gmail.com>
2025-06-21 16:33:20 +0700

    11: add error handling

--- 11-price_calculator/main.go
+++ 11-price_calculator/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"example.com/price_calculator/cmdmanager"
+	"example.com/price_calculator/filemanager"
 	"example.com/price_calculator/prices"
+	"fmt"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		//fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		priceJob.Process()
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		//cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+		if err != nil {
+			fmt.Println("Could not procress job!")
+			fmt.Println(err)
+		}
 	}
-
 }
