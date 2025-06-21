47c0d47dcaea4da2f48684e04be514a4840555f6
657002 <imnew108@gmail.com>
2025-06-21 14:17:56 +0700

    11: work with the file Data

--- 11-price_calculator/main.go
+++ 11-price_calculator/main.go
@@ -1,27 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
-	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	//taxRates := []float64{0, 0.07, 0.1, 0.15}
+	//
+	//for _, taxRate := range taxRates {
+	//	priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+	//	priceJob.Process()
+	//}
 
-	result := make(map[float64][]float64)
-
-	for _, taxRate := range taxRates {
-		//fmt.Println("Tax Rate:", val)
-
-		taxIncludedPrices := make([]float64, len(prices))
-		for i, p := range prices {
-
-			taxIncludedPrices[i] = p * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
+	price, err := os.ReadFile("prices.txt")
+	if err != nil {
+		fmt.Println(errors.New("Can not read prices from prices.txt"))
 	}
 
-	for k, v := range result {
-		fmt.Printf("Tax Rate: %.2f\n", k)
-		fmt.Printf("Price: %.2f\n", v)
-	}
+	fmt.Println(price)
 }
--- 11-price_calculator/prices/prices.go
+++ 11-price_calculator/prices/prices.go
@@ -1,9 +1,56 @@
 package prices
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println(errors.New("Cannot open file prices.txt!"))
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file) // todo เป็นการเปิดเผยการใช้เนื้อหา
+
+	var lines []string
+
+	for scanner.Scan() { // todo อ่านค่าใน file แบบ line by line จึงใช้ for เพื่อ scan ทุกบรรทัด (response type: bool)
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err() // scanner.Scan() บอกแค่ true false จึงต้องใช้ scanner.Err() ในการเช็ค err
+	if err != nil {
+		fmt.Println(errors.New("Reading the file content failed."))
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64, len(job.TaxIncludedPrices))
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+
+	for k, v := range result {
+		fmt.Printf("Price: %s\n", k)
+		fmt.Printf("Price Included Tax: %v\n", v)
+	}
+
+	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
