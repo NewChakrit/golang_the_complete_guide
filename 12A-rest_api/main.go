6f0a4e6ce65731f395dc9800d8c5dbd3448bf9da
657002 <imnew108@gmail.com>
2025-06-22 13:59:06 +0700

    12A: setting up an event model and registering a POST route

--- 12A-rest_api/main.go
+++ 12A-rest_api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"example.com/rest_api/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,12 +10,28 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{ // H = map[string]any
-		"message": "Hello!",
-	})
+	evetns := models.GetAllEvents()
+	//context.JSON(http.StatusOK, gin.H{ // H = map[string]any
+	//	"message": "Hello!",
+	//})
+
+	context.JSON(http.StatusOK, evetns)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data."})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
